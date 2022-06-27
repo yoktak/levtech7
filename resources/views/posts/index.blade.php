c423b46c31792a0ed2bd02337bacc22bf93fc219
yoktak <yokotaku327@gmail.com>
2022-06-27 17:33:28 +0900

    ブログ投稿作成画面の作成

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <p class='create'>[<a href='/posts/create'>create</a>]</p>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <a href='/posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+    </body>
+</html>
